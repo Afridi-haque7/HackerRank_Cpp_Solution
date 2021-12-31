55a59efd606528ece2232a2e63b02289bcb865eb
AFRIDI HAQUE <85033515+Afridi-haque7@users.noreply.github.com>
2021-12-31 15:19:56 +0530

    Add files via upload

---
+++ Vector-Sort.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    
+    for(int i=0; i<n; i++){
+        int input;
+        cin>>input;
+        v.push_back(input);
+    }  
+    
+    sort(v.begin(),v.end());
+    
+    //for(int i=0; i<n; i++)
+    for(int j:v){
+        cout<<j<<" ";        
+    } 
+    return 0;
+}
+
